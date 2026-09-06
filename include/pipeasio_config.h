bb7911e0e7590a76c11b9b9843735fb116b9c1bf
M0n7y5 <17201053+M0n7y5@users.noreply.github.com>
2026-09-06 20:50:53 +0200

    chore(release): 1.7.0

--- include/pipeasio_config.h
+++ include/pipeasio_config.h
@@ -30,7 +30,7 @@
 #include <stdint.h>
 
 /* --- Product version (driver log + settings-panel title) ------------------ */
-#define PIPEASIO_VERSION "1.6.0"
+#define PIPEASIO_VERSION "1.7.0"
 
 /* --- File location (relative to $XDG_CONFIG_HOME, else $HOME/.config) ----- */
 #define PIPEASIO_CONFIG_DIR "pipeasio"
